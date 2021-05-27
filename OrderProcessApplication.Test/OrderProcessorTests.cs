6c1cf50082898d753c73a543c100e9015f3cd28f
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-05-28 00:11:11 +0200

    Add OrderProcessor and test

--- OrderProcessApplication.Test/OrderProcessorTests.cs
+++ OrderProcessApplication.Test/OrderProcessorTests.cs
@@ -1,4 +1,6 @@
 using System;
+using OrderProcessorApplication;
+using OrderProcessorApplication.Models;
 using Xunit;
 
 namespace OrderProcessApplication.Test
@@ -6,9 +8,22 @@ namespace OrderProcessApplication.Test
     public class OrderProcessorTests
     {
         [Fact]
-        public void Test1()
+        public void Should_Process_Order()
         {
+            // Arrange
+            var sut = CreateDefaultOrderProcessor();
+            var order = Order.Create(new NonPhysicalProduct());
 
+            // Act
+            sut.Process(order);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        private OrderProcessor CreateDefaultOrderProcessor()
+        {
+            return new();
         }
     }
 }
---
+++ OrderProcessorApplication/OrderProcessor.cs
@@ -0,0 +1,13 @@
+﻿using OrderProcessorApplication.Models;
+
+namespace OrderProcessorApplication
+{
+    public class OrderProcessor
+    {
+        public void Process(Order order)
+
+        {
+            
+        }
+    }
+}
