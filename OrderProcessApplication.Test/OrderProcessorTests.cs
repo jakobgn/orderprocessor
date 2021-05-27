62413d2f9e0955c6098810591f21e7195ca5b3f0
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-05-27 23:33:42 +0200

    init

---
+++ OrderProcessApplication.Test/OrderProcessorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OrderProcessApplication.Test
+{
+    public class OrderProcessorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
