43de234d6bab25706a8ca0aeb9976ecee8a86713
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-05-28 09:21:49 +0200

    Added unit test for BookHandler

---
+++ OrderProcessApplication.Test/BookHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OrderProcessorApplication.Handlers;
+using OrderProcessorApplication.Models;
+using Xunit;
+
+namespace OrderProcessApplication.Test
+{
+    public class BookHandlerTests
+    {
+        [Fact]
+        public void Should_Generate_Duplicate_Packing_Slip_When_Book()
+        {
+            // Arrange
+            var mock = new Mock<IPackingService>();
+            var order = Order.Create(new Book(""));
+            var sut = new BookHandler(mock.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mock.Verify(m => m.GenerateDuplicatePackingSlip(), Times.Once);
+        }
+
+        [Fact]
+        public void Should_Not_Generate_Duplicate_Packing_Slip_When_Not_Book()
+        {
+            // Arrange
+            var mock = new Mock<IPackingService>();
+            var order = Order.Create(new PhysicalProduct());
+            var sut = new BookHandler(mock.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mock.Verify(m => m.GenerateDuplicatePackingSlip(), Times.Never);
+        }
+    }
+}
