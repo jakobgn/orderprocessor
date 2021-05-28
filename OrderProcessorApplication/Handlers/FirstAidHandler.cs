eed7b22805a69462280b5a33348815aa0ebbef5c
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-05-28 11:39:44 +0200

    Add first aid handler

---
+++ OrderProcessorApplication/Handlers/FirstAidHandler.cs
@@ -0,0 +1,24 @@
+﻿using OrderProcessorApplication.Models;
+using OrderProcessorApplication.Services;
+
+namespace OrderProcessorApplication.Handlers
+{
+    public class FirstAidHandler : HandlerBase<Order>
+    {
+        private readonly IPackingService _packingService;
+
+        public FirstAidHandler(IPackingService packingService)
+        {
+            _packingService = packingService;
+        }
+        public override void Process(Order order)
+        {
+            if (order.Product is Video && order.Product.Name.Equals("Learning to Ski"))
+            {
+                _packingService.GenerateFirstAidSlip();
+            }
+            base.Process(order);
+        }
+ 
+    }
+}
