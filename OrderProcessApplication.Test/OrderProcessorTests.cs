2d7b5e867a733dbf5bc0ce301c9d47aff221692f
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-05-28 11:16:40 +0200

    Integrate membership and add few more tests

--- OrderProcessApplication.Test/OrderProcessorTests.cs
+++ OrderProcessApplication.Test/OrderProcessorTests.cs
@@ -20,7 +20,48 @@ namespace OrderProcessApplication.Test
             // Assert
             Assert.True(true);
         }
+        [Fact]
+        public void Should_Process_Physical_Order()
+        {
+            // Arrange
+            var sut = CreateDefaultOrderProcessor();
+            var order = Order.Create(new PhysicalProduct());
 
+            // Act
+            sut.Process(order);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Should_Process_Book_Order()
+        {
+            // Arrange
+            var sut = CreateDefaultOrderProcessor();
+            var order = Order.Create(new Book("Harry potter"));
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            Assert.True(true);
+        }
+
+
+        [Fact]
+        public void Should_Process_Membership_Order()
+        {
+            // Arrange
+            var sut = CreateDefaultOrderProcessor();
+            var order = Order.Create(new MembershipActivation(), "test@abc.com");
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            Assert.True(true);
+        }
         private OrderProcessor CreateDefaultOrderProcessor()
         {
             return new();
--- OrderProcessorApplication/OrderProcessorFactory.cs
+++ OrderProcessorApplication/OrderProcessorFactory.cs
@@ -7,17 +7,22 @@ namespace OrderProcessorApplication
     public class OrderProcessorFactory
     {
         private readonly PackingService _packingService;
+        private MembershipService _membershipService;
 
         public OrderProcessorFactory()
         {
             _packingService = new PackingService();
+            _membershipService = new MembershipService();
         }
         public IHandler<Order> CreateRules()
         {
             var physicalProductHandler = new PhysicalProductHandler(_packingService);
             var bookHandler = new BookHandler(_packingService);
-
+            var membershipActivationHandler = new MembershipActivationHandler(_membershipService);
+            var membershipUpgradeHandler = new MembershipUpgradeHandler(_membershipService);
             physicalProductHandler.SetNext(bookHandler);
+            bookHandler.SetNext(membershipActivationHandler);
+            membershipUpgradeHandler.SetNext(membershipUpgradeHandler);
             return physicalProductHandler;
         }
     }
