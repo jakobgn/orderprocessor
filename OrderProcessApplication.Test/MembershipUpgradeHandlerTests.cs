76158d2503429afb8e7ce7834057550c73b3eaf2
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-05-28 10:54:50 +0200

    Add membership upgrade

---
+++ OrderProcessApplication.Test/MembershipUpgradeHandlerTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OrderProcessorApplication.Handlers;
+using OrderProcessorApplication.Models;
+using OrderProcessorApplication.Services;
+using Xunit;
+
+namespace OrderProcessApplication.Test
+{
+    public class MembershipUpgradeHandlerTests
+    {
+        [Fact]
+        public void Should_Activate_When_MembershipUpgrade()
+        {
+            // Arrange
+            var mock = new Mock<IMembershipService>();
+            var order = Order.Create(new MembershipUpgrade());
+            var sut = new MembershipUpgradeHandler(mock.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mock.Verify(m => m.Upgrade(), Times.Once);
+        }
+
+        [Fact]
+        public void Should_Send_Email_When_MembershipUpgrade()
+        {
+            // Arrange
+            var mock = new Mock<IMembershipService>();
+            var order = Order.Create(new MembershipUpgrade());
+            var sut = new MembershipUpgradeHandler(mock.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mock.Verify(m => m.SendEmail(order.Email), Times.Once);
+        }
+
+        [Fact]
+        public void Should_Not_Activate_When_Not_MembershipUpgrade()
+        {
+            // Arrange
+            var mock = new Mock<IMembershipService>();
+            var order = Order.Create(new PhysicalProduct());
+            var sut = new MembershipUpgradeHandler(mock.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mock.Verify(m => m.Upgrade(), Times.Never);
+        }
+
+        [Fact]
+        public void Should_Not_Send_Email_When_Not_MembershipUpgrade()
+        {
+            // Arrange
+            var mock = new Mock<IMembershipService>();
+            var order = Order.Create(new PhysicalProduct());
+            var sut = new MembershipUpgradeHandler(mock.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mock.Verify(m => m.SendEmail(order.Email), Times.Never);
+        }
+
+        [Fact]
+        public void Should_Call_Next_Handler_When_MembershipUpgrade()
+        {
+            // Arrange
+            var order = Order.Create(new MembershipUpgrade());
+            var sut = new MembershipUpgradeHandler(new Mock<IMembershipService>().Object);
+            var mockHandler = new Mock<IHandler<Order>>();
+            sut.SetNext(mockHandler.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mockHandler.Verify(m => m.Process(order), Times.Once);
+        }
+
+        [Fact]
+        public void Should_Call_Next_Handler_When_Not_MembershipUpgrade()
+        {
+            // Arrange
+            var order = Order.Create(new PhysicalProduct());
+            var sut = new MembershipUpgradeHandler(new Mock<IMembershipService>().Object);
+            var mockHandler = new Mock<IHandler<Order>>();
+            sut.SetNext(mockHandler.Object);
+
+            // Act
+            sut.Process(order);
+
+            // Assert
+            mockHandler.Verify(m => m.Process(order), Times.Once);
+        }
+    }
+}
---
+++ OrderProcessorApplication/Handlers/MembershipUpgradeHandler.cs
@@ -0,0 +1,24 @@
+﻿using OrderProcessorApplication.Models;
+using OrderProcessorApplication.Services;
+
+namespace OrderProcessorApplication.Handlers
+{
+    public class MembershipUpgradeHandler : HandlerBase<Order>
+    {
+        private readonly IMembershipService _membershipService;
+
+        public MembershipUpgradeHandler(IMembershipService membershipService)
+        {
+            _membershipService = membershipService;
+        }
+        public override void Process(Order order)
+        {
+            if (order.Product is MembershipUpgrade)
+            {
+                _membershipService.Upgrade();
+                _membershipService.SendEmail(order.Email);
+            }
+            base.Process(order);
+        }
+    }
+}
